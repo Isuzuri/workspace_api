4f495f140b2526abe26029d1701ec5fcbd16635f
Isuzu <ballage94@gmail.com>
2026-06-05 14:09:12 +0300

    add jti field + miinof fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, path: "", path_names: {
     sign_in: "login",
     sign_out: "logout",
-    registration: "sighup"
+    registration: "signup"
   }, controllers: {
-    registration: "users/registration",
+    registration: "users/registrations",
     sessions: "users/sessions"
   }
 end
